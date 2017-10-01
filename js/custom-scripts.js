a1d7117e14ab3a6574886a07c544f3ae1b6ead71
Viktor Maksymenko <vikmaksimenko@hotmail.com>
2017-10-01 16:39:38 +0300

    Completed work on site

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -215,6 +215,146 @@
 
   });
 
+  $.fn.scrollView = function () {
+    return this.each(function () {
+      $('html, body').animate({
+        scrollTop: $(this).offset().top
+      }, 500);
+    });
+  }
+
+  $("a.btn-nav").click(function(event) {
+    var target = event.target;
+    var anchor = $(target).attr("href");
+    $(anchor).scrollView();
+  });
+
+
 
 } (jQuery) );
+
+
+function initMap() {
+    // Styles a map in night mode.
+    var position = { lat: 45.342315, lng: 28.837835 }
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: position,
+        // , 
+        zoom: 17,
+        styles: [
+
+            {
+                "featureType": "administrative",
+                "elementType": "all",
+                "stylers": [{
+                    "saturation": "-100"
+                }]
+            },
+            {
+                "featureType": "administrative.province",
+                "elementType": "all",
+                "stylers": [{
+                    "visibility": "off"
+                }]
+            },
+            {
+                "featureType": "landscape",
+                "elementType": "all",
+                "stylers": [{
+                        "saturation": -100
+                    },
+                    {
+                        "lightness": 65
+                    },
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "all",
+                "stylers": [{
+                        "saturation": -100
+                    },
+                    {
+                        "lightness": "50"
+                    },
+                    {
+                        "visibility": "simplified"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "all",
+                "stylers": [{
+                    "saturation": "-100"
+                }]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "all",
+                "stylers": [{
+                    "visibility": "simplified"
+                }]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "all",
+                "stylers": [{
+                    "lightness": "30"
+                }]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "all",
+                "stylers": [{
+                    "lightness": "40"
+                }]
+            },
+            {
+                "featureType": "transit",
+                "elementType": "all",
+                "stylers": [{
+                        "saturation": -100
+                    },
+                    {
+                        "visibility": "simplified"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [{
+                        "hue": "#ffff00"
+                    },
+                    {
+                        "lightness": -25
+                    },
+                    {
+                        "saturation": -97
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "labels",
+                "stylers": [{
+                        "lightness": -25
+                    },
+                    {
+                        "saturation": -100
+                    }
+                ]
+            }
+        ]
+    });
+    var marker = new google.maps.Marker({
+        position: position,
+        map: map,
+        icon: "icons/google-maps-grey-marker-w-shadow.png"
+    });
+}
 
